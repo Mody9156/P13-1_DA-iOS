a747125c949f295b3d1d2141cd6c6b688bc99602
Modibo KEITA <modykeita2@gmail.com>
2024-11-07 17:16:24 +0100

    update function

--- Relayance/RelayanceTests/Model/ClientTests.swift
+++ Relayance/RelayanceTests/Model/ClientTests.swift
@@ -12,20 +12,24 @@ final class ClientTests: XCTestCase {
     
     func testClientInitialization_WithValidData_ContainsExpectedClients(){
         
+        //Given
         let nom  = "Malala Yousafzai"
         let email = "malala.yousafzai@example.com"
         let dateCreationString = "2022-03-10T10:45:00Z"
         var dateCreation: Date {
             Date.dateFromString(dateCreationString) ?? Date.now
         }
-
+        //When
         let client = Client(nom: nom, email: email, dateCreationString: dateCreationString)
         
+        //Then
         XCTAssertEqual(client.nom, nom)
         XCTAssertEqual(client.email, email)
         XCTAssertEqual(client.dateCreation, dateCreation)
        
     }
+    
+    
 
 
 }
