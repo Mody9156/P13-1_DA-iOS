2b915a544c76eb2088795c4632f0a18064c0fc2b
Modibo KEITA <modykeita2@gmail.com>
2024-11-07 16:49:16 +0100

    created new methode with right infos

--- Relayance/RelayanceTests/Model/ClientTests.swift
+++ Relayance/RelayanceTests/Model/ClientTests.swift
@@ -6,30 +6,24 @@
 //
 
 import XCTest
+@testable import Relayance
 
 final class ClientTests: XCTestCase {
+    
+    func testClientInitialization_WithValidData_ContainsExpectedClients(){
+        
+        var nom  = "Malala Yousafzai"
+        var email = "malala.yousafzai@example.com"
+        var dateCreationString = "2022-03-10T10:45:00Z"
+          
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let client = Client(nom: nom, email: email, dateCreationString: dateCreationString)
+        
+        XCTAssertEqual(client.nom, nom)
+        XCTAssertEqual(client.email, email)
+        XCTAssertEqual(client.dateCreation, Date())
+       
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
