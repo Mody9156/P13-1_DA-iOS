7e6750d3c1db8115c5546a66d0a3e21a65266f76
Modibo KEITA <modykeita2@gmail.com>
2024-11-08 12:12:13 +0100

    add new viewmodel

---
+++ Relayance/Relayance/ViewModel/ClientManagementViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ClientManagementViewModel.swift
+//  Relayance
+//
+//  Created by KEITA on 08/11/2024.
+//
+
+import Foundation
+
+class ClientManagementViewModel : ObservableObject {
+    
+}
