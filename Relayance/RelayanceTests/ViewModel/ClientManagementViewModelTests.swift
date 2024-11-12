4c85ab0b49d2210c059e695640da314f7e5a987e
Modibo KEITA <modykeita2@gmail.com>
2024-11-12 10:29:20 +0100

    add assert

--- Relayance/RelayanceTests/ViewModel/ClientManagementViewModelTests.swift
+++ Relayance/RelayanceTests/ViewModel/ClientManagementViewModelTests.swift
@@ -11,9 +11,16 @@ final class ClientManagementViewModelTests: XCTestCase {
 
     
     func testWhenAddNewClient() async throws{
+        //Given
         let dateNow = Date.now
         let client = Client(nom: "James", email: "JamesBrown@gmail.com", dateCreationString: "2022-03-10T10:45:00Z")
         let viewModel = ClientManagementViewModel(client: client )
+        
+        //When
+        let newClient = try await viewModel.addNewClient()
+        //Then
+        XCTAssertEqual(newClient[1].nom, "James")
+        XCTAssertEqual(newClient[1].email, "JamesBrown@gmail.com")
         
     }
 
