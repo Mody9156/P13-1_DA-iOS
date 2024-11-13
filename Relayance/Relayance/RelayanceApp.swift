143d9489a36300fb34ed9e6fefe9041d4c89af0e
Modibo KEITA <modykeita2@gmail.com>
2024-11-13 18:02:55 +0100

    use variable

--- Relayance/Relayance/RelayanceApp.swift
+++ Relayance/Relayance/RelayanceApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct RelayanceApp: App {
     var body: some Scene {
         WindowGroup {
-            ListClientsView( clientManagementViewModel: ClientManagementViewModel(client: Client(nom: "", email: "", dateCreationString: "")))
+            ListClientsView( clientManagementViewModel: ClientManagementViewModel(client: Client(nom: "", email: "", dateCreationString: ""), message: ""))
         }
     }
 }
